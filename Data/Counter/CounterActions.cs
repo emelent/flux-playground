e3e03461e83a0d32091d6a530e5626f53ff97c52
Roy <roy.motloutsi@gmail.com>
2023-04-04 20:20:30 +0200

    clean up

--- Data/Counter/CounterActions.cs
+++ Data/Counter/CounterActions.cs
@@ -1,8 +1,5 @@
 namespace FluxorSample.Data.Counter;
 
-// public record CounterUpdatedAction(int Value);
-// public record CounterUpdateFailedAction(string ErrorMessage);
-
 public record CounterIncrement<K>(
    K ActionKind
 ) where K : ActionKind;
